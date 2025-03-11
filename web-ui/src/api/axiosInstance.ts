d46e516f710e25dd45deadc63534ea6096a84856
Torrixo <santy.torrico@gmail.com>
2025-03-11 19:03:08 -0400

    changed url for production

--- web-ui/src/api/axiosInstance.ts
+++ web-ui/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `https:todo-app-production-e398.up.railway.app/api`;
+const API_URL = "https:todo-app-production-e398.up.railway.app/api";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
