8894c31beb7cb33d79d268819f2135ccca095128
Torrixo <santy.torrico@gmail.com>
2025-03-11 17:26:10 -0400

    Configure for Render deployment

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,10 @@ import aiRoutes from "./routes/aiRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*',
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
