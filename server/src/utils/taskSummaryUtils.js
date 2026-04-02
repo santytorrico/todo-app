069705ac65c9e2c8e63544a0d76823c1972b7a85
santytorrico <santy.torrico@gmail.com>
2026-04-02 13:18:30 -0400

    feat: add logic to get smaller chunks on the response

--- server/src/utils/taskSummaryUtils.js
+++ server/src/utils/taskSummaryUtils.js
@@ -54,7 +54,15 @@ export const generateSummaryStream = async (content, onChunk) => {
             const chunkText = chunk.text();
             // Remove markdown formatting characters
             const cleanText = chunkText.replace(/[*_`]/g, "");
-            onChunk(cleanText);
+
+            const words = cleanText.split(/(\s+)/);
+            for(const word of words){
+                if(word){
+                    onChunk(word);
+                    await new Promise(resolve => setTimeout(resolve, 30));
+                }
+            }
+            // onChunk(cleanText);
         }
     } catch (error) {
         console.error("Gemini Streaming Error:", error.message);
